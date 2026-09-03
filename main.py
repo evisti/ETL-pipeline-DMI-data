6379f49752937030ed0f267fe3d9481638a704c7
Emilie <emilie.vistisen@gmail.com>
2026-09-03 12:32:10 +0200

    Create init file

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import os
-import pandas as pd
-
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from sqlalchemy import MetaData
 
+import etl
 from db_connection import SQLRunner, get_engine
-
-from etl.extract import StationExtractor, ObservationExtractor
-from etl.transform import StationTransformer, ObservationTransformer
-from etl.load import Loader
-from etl.etl import ETLPipeline
-
-from etl.tables import station_table, observation_table
 
 
 # Load environment variables
@@ -26,25 +18,33 @@ DATABASE = os.getenv('DATABASE')
 
 # Parameter definitions
 from_time = datetime(2025, 1, 1)
-to_time = from_time + timedelta(days=5)
+to_time = from_time + timedelta(days=1)
 station_id = '06072'
 parameters = None # all parameters
 
 # SQL connection
-sql_runner = SQLRunner(get_engine(user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE))
+sql_runner = SQLRunner(
+    get_engine(
+        user=USER, 
+        password=PASSWORD, 
+        host=HOST, 
+        port=PORT, 
+        database=DATABASE
+    )
+)
 
 # SQL table metadata
 metadata = MetaData()
 
 
 def run_etl_observations():
-    table = observation_table(metadata, name='observations')
+    table = etl.observation_table(metadata, name='observations')
 #    sql_runner.create_tables(metadata)
 
-    pipeline = ETLPipeline(
-        extractor=ObservationExtractor(DMI_URL, station_id, parameters, from_time, to_time), 
-        transformer=ObservationTransformer(), 
-        loader=Loader(sql_runner, table)
+    pipeline = etl.ETLPipeline(
+        extractor=etl.ObservationExtractor(DMI_URL, station_id, parameters, from_time, to_time), 
+        transformer=etl.ObservationTransformer(), 
+        loader=etl.Loader(sql_runner, table)
     )
     df = pipeline.run(dry_run=True)
     print(df.head())
@@ -52,13 +52,13 @@ def run_etl_observations():
 
 def run_etl_stations():
 
-    table = station_table(metadata, name='station_test')
+    table = etl.station_table(metadata, name='station_test')
     #sql_runner.create_tables(metadata)
 
-    pipeline = ETLPipeline(
-        extractor=StationExtractor(DMI_URL), 
-        transformer=StationTransformer(), 
-        loader=Loader(sql_runner, table)
+    pipeline = etl.ETLPipeline(
+        extractor=etl.StationExtractor(DMI_URL), 
+        transformer=etl.StationTransformer(), 
+        loader=etl.Loader(sql_runner, table)
     )
     df = pipeline.run()
     print(df.head())
@@ -71,3 +71,4 @@ def main():
 
 if __name__=='__main__':
     run_etl_observations()
+    
